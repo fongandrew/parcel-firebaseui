78284c0ef6f7368ecbd59dd155522c77f15a46aa
Andrew Fong <id@andrewfong.com>
2019-08-13 22:48:15 -0700

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,6 @@
+require("firebase");
+const ui = require("firebaseui");
+const dialogPolyfill = require("dialog-polyfill");
+
+console.log("firebaseui", ui);
+console.log("dialog-polyfill", dialogPolyfill);
